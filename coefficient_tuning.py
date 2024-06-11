e7288b23696d8c240ebf5575cf4ab1d867326f1a
Gallinator <lcgallinator4@gmail.com>
2024-06-11 11:53:38 +0200

    coefficient_tuning: faster tuning, needs more testing

--- coefficient_tuning.py
+++ coefficient_tuning.py
@@ -1,6 +1,4 @@
 import math
-import random
-
 import numpy as np
 import scipy
 
@@ -20,71 +18,56 @@ def get_oom(value):
 
 class DataGenerator:
     def __init__(self, aircraft: AircraftData,
-                 container_num_range: tuple,
-                 container_mass_range: tuple,
-                 max_payload: float,
-                 zero_payload_mass_range: tuple,
+                 container_masses: np.ndarray,
+                 container_types: np.ndarray,
                  num_slack_variables: dict):
         self.aircraft = aircraft
-        self.cont_num_max = container_num_range[1]
-        self.cont_num_min = container_num_range[0]
-        self.cont_mass_max = container_mass_range[1]
-        self.cont_mass_min = container_mass_range[0]
-        self.zero_pl_max = zero_payload_mass_range[1]
-        self.zero_pl_min = zero_payload_mass_range[0]
-        self.max_pl = max_payload
+        self.cont_num = len(container_types)
+        self.cont_masses = container_masses
         self.num_slack = num_slack_variables
+        self.cont_types = container_types
+        self.cont_t = get_container_t(container_types)
+        self.cont_d = get_container_d(container_types)
 
-    def generate(self, num_samples) -> np.ndarray:
+    def generate(self, num_samples) -> np.array:
         data = np.empty(shape=num_samples, dtype=dict)
-
         for n in range(num_samples):
             d = {}
-            n_cont = random.randint(self.cont_num_min, self.cont_num_max)
-
-            d['cont_occ'] = generate_random_sparse_occupancies(n_cont, self.aircraft.num_positions)
-
-            cont_types = np.random.choice(['t1', 't2', 't3'], n_cont)
-            d['cont_types'] = cont_types
-            d['cont_d'] = get_container_d(cont_types)
-            d['cont_t'] = get_container_t(cont_types)
-            d['cont_masses'] = np.random.randint(low=self.cont_mass_min, high=self.cont_mass_max, size=n_cont)
-
-            d['max_pl'] = self.max_pl
+            d['cont_occ'] = generate_random_sparse_occupancies(self.cont_num, self.aircraft.num_positions)
+            d['cont_types'] = self.cont_types
+            d['cont_masses'] = self.cont_masses
+            d['cont_d'] = self.cont_d
+            d['cont_t'] = self.cont_t
+            d['max_pl'] = self.aircraft.max_payload
 
             slacks = {}
             for v_k in self.num_slack.keys():
                 slacks[v_k] = np.random.randint(low=0, high=2, size=self.num_slack[v_k])
             d['slack_vars'] = slacks
-
             data[n] = d
 
         return data
 
 
 def tune_coef(data_generator: DataGenerator,
-              num_samples: int,
-              step_increase: float,
+              num_samples: float,
               objective_f,
               penalty_f) -> float:
-    penalty_oom = -1.0
-    obj_oom = 0.0
-    coef = 0.0
+    penalty_outs = []
+    obj_outs = []
 
-    while penalty_oom < obj_oom:
-        coef += step_increase
-        penalty_outs = []
-        obj_outs = []
-        data = data_generator.generate(num_samples)
+    for s in data_generator.generate(num_samples):
+        obj_outs.append(objective_f(**s))
+        penalty_outs.append(penalty_f(**s))
 
-        for _, s in enumerate(data):
-            obj_outs.append(objective_f(**s))
-            penalty_outs.append(coef * penalty_f(**s))
+    obj_mean = np.mean(obj_outs)
+    obj_oom = get_oom(abs(obj_mean))
+    penalty_mean = np.mean(penalty_outs)
+    penalty_oom = get_oom(abs(penalty_mean))
+    if penalty_mean == 0:
+        coef = 0
+    else:
+        coef = math.pow(10, obj_oom - penalty_oom)
 
-        obj_oom = get_oom(abs(np.mean(obj_outs)))
-        penalty_oom = get_oom(abs(np.mean(penalty_outs)))
-
-        print(f'{penalty_f.__name__}\n Penalty oom: {penalty_oom}, Objective oom: {obj_oom}, Coefficient: {coef}\n')
-
-    print(f'Calculated coefficient: {coef}')
+    print(f'{penalty_f.__name__}\n Penalty mean: {penalty_mean}, Objective mean: {obj_mean}, Coefficient: {coef}\n')
     return coef
