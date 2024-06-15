a901cf3c5423039736fada74c749e26cb867b3e6
Gallinator <lcgallinator4@gmail.com>
2024-06-15 17:52:57 +0200

    AircraftData: add container location ends tests

--- tests/aircraft_data_tests.py
+++ tests/aircraft_data_tests.py
@@ -12,3 +12,10 @@ class AircraftDataTests(unittest.TestCase):
         locations = np.array([-19, -17, -15, -13, -11, -9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
 
         self.assertEqual(acft.locations.all(), locations.all())
+
+    def test_location_ends(self):
+        acft = get_default_aircraft()
+
+        locations = np.array([-18, -16, 14, -12, -10, -8, -6, 4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+
+        self.assertEqual(acft.location_ends.all(), locations.all())
