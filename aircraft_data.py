b57d022d1e68a9af5243d80118083618bb4950c1
Gallinator <lcgallinator4@gmail.com>
2024-06-15 17:52:43 +0200

    AircraftData: calculate container location ends

--- aircraft_data.py
+++ aircraft_data.py
@@ -10,6 +10,7 @@ class AircraftData:
         self.min_cg = min_cg * payload_area_length
         self.max_cg = max_cg * payload_area_length
         self.locations = self.get_locations()
+        self.location_ends = self.get_location_ends()
 
         if min_cg > max_cg:
             raise ValueError(f'Invalid cg limits ({min_cg}>{max_cg})')
@@ -21,6 +22,13 @@ class AircraftData:
                    ((1 + i) - self.num_positions / 2) -
                    self.payload_area_length / (2 * self.num_positions))
             pos.append(x_i)
+        return np.array(pos)
+
+    def get_location_ends(self) -> np.ndarray:
+        pos = []
+        for u in range(self.num_positions):
+            x_u = self.payload_area_length / self.num_positions * ((u + 1) - self.num_positions / 2)
+            pos.append(x_u)
         return np.array(pos)
 
 
