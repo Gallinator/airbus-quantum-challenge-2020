ade0a222534b078ff3b4fcaa32574d8655ddfb8d
Gallinator <lcgallinator4@gmail.com>
2024-06-21 18:54:09 +0200

    coefficient_tuning: pas BQM instead of qubo problems

--- coefficient_tuning.py
+++ coefficient_tuning.py
@@ -1,9 +1,10 @@
 import math
 import numpy as np
+from dimod import BQM
 
 
-def get_coef(objective_q: dict, penalty_q: dict) -> float:
-    penalty_values = list(penalty_q.values())
+def get_coef(objective_q: BQM, penalty_q: BQM) -> float:
+    penalty_values = list(penalty_q.to_qubo()[0].values())
 
     if len(penalty_values) == 0:
         return 0
@@ -12,6 +13,6 @@ def get_coef(objective_q: dict, penalty_q: dict) -> float:
     if penalty_mean == 0:
         return 0
 
-    obj_mean = np.mean(list(objective_q.values()))
+    obj_mean = np.mean(np.abs(list(objective_q.to_qubo()[0].values())))
 
     return math.pow(10, math.log10(obj_mean) - math.log10(penalty_mean))
