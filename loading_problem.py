890b2893035f08412f039938255c239d8c0dd0f3
Gallinator <lcgallinator4@gmail.com>
2024-07-07 20:27:36 +0200

    LoadingProblem: add shear limit coefficients

--- loading_problem.py
+++ loading_problem.py
@@ -26,7 +26,15 @@ class LoadingProblem:
         self.zero_payload_cg = zero_payload_cg * aircraft.payload_area_length
         self.container_t = get_container_t(container_types)
         self.container_d = get_container_d(container_types)
-        self.coefficients = {'pl_o': 1.0, 'pl_w': 1.0, 'pl_d': 1.0, 'pl_c': 1.0, 'cl_t': 1.0, 'cl_u': 1.0, 'cl_l': 1.0}
+        self.coefficients = {'pl_o': 1.0,
+                             'pl_w': 1.0,
+                             'pl_d': 1.0,
+                             'pl_c': 1.0,
+                             'cl_t': 1.0,
+                             'cl_u': 1.0,
+                             'cl_l': 1.0,
+                             'sl_l': 1.0,
+                             'sl_r': 1.0}
         self.num_slack_variables = self.get_num_slack_vars()
 
     def get_objective_bqm(self) -> BQM:
