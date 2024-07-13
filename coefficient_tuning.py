1a243e7f42dcab750070a50d0355a3e520a00cc9
Gallinator <lcgallinator4@gmail.com>
2024-07-13 14:52:25 +0200

    coefficient_tuning: add average and iterative functions

--- coefficient_tuning.py
+++ coefficient_tuning.py
@@ -3,8 +3,51 @@ import numpy as np
 from dimod import BQM
 from dwave.samplers import SteepestDescentSampler
 
+from loading_problem import LoadingProblem
 
-def get_coef(objective_q: BQM, penalty_q: BQM) -> float:
+
+def tune_coefs_average(problem: LoadingProblem) -> dict:
+    coefs = {}
+    coefs['pl_o'] = calc_coef_average(problem.get_objective_bqm(), problem.get_no_overlaps_bqm())
+    coefs['pl_w'] = calc_coef_average(problem.get_objective_bqm(), problem.get_max_capacity_bqm())
+    coefs['pl_d'] = calc_coef_average(problem.get_objective_bqm(), problem.get_no_duplicates_bqm())
+    coefs['pl_c'] = calc_coef_average(problem.get_objective_bqm(), problem.get_contiguity_bqm())
+    coefs['cl_u'] = calc_coef_average(problem.get_objective_bqm(), problem.get_cg_upper_bqm())
+    coefs['cl_l'] = calc_coef_average(problem.get_objective_bqm(), problem.get_cg_lower_bqm())
+    coefs['cl_t'] = calc_coef_average(problem.get_objective_bqm(), problem.get_cg_target_bqm())
+    coefs['sl_l'] = calc_coef_average(problem.get_objective_bqm(), problem.get_left_shear_bqm())
+    coefs['sl_r'] = calc_coef_average(problem.get_objective_bqm(), problem.get_right_shear_bqm())
+    return coefs
+
+
+def tune_coefs_iterative(problem: LoadingProblem, step_sizes=None, verbose=True) -> dict:
+    if step_sizes is None:
+        step_sizes = {'pl_o': 1, 'pl_w': 0.01, 'pl_d': 0.1, 'pl_c': 1,
+                      'cl_t': 0.0000000001, 'cl_u': 0.000000001, 'cl_l': 0.0000000001,
+                      'sl_l': 0.1, 'sl_r': 0.01}
+    coefs = {}
+    coefs['pl_o'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_no_overlaps_bqm(),
+                                        step_sizes['pl_o'], verbose)
+    coefs['pl_w'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_max_capacity_bqm(),
+                                        step_sizes['pl_w'], verbose)
+    coefs['pl_d'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_no_duplicates_bqm(),
+                                        step_sizes['pl_d'], verbose)
+    coefs['pl_c'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_contiguity_bqm(),
+                                        step_sizes['pl_c'], verbose)
+    coefs['cl_u'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_cg_upper_bqm(),
+                                        step_sizes['cl_u'], verbose)
+    coefs['cl_l'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_cg_lower_bqm(),
+                                        step_sizes['cl_l'], verbose)
+    coefs['cl_t'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_cg_target_bqm(),
+                                        step_sizes['cl_t'], verbose)
+    coefs['sl_l'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_left_shear_bqm(),
+                                        step_sizes['sl_l'], verbose)
+    coefs['sl_r'] = calc_coef_iterative(problem.get_objective_bqm(), problem.get_right_shear_bqm(),
+                                        step_sizes['sl_r'], verbose)
+    return coefs
+
+
+def calc_coef_average(objective_q: BQM, penalty_q: BQM) -> float:
     penalty_values = list(penalty_q.to_qubo()[0].values())
 
     if len(penalty_values) == 0:
@@ -25,7 +68,7 @@ def get_oom(value: float) -> int:
     return math.floor(math.log10(abs(value)))
 
 
-def tune_coef(obj_bqm: BQM, penalty_bqm: BQM, step_size, verbose=False):
+def calc_coef_iterative(obj_bqm: BQM, penalty_bqm: BQM, step_size, verbose=False):
     print(f'Tuning {penalty_bqm.shape}')
     sampler = SteepestDescentSampler()
     prev_state = None
